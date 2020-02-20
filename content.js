2d78883ceb340d8a3056998df0630993798a0447
Rodion Chachura <geekrodion@gmail.com>
2020-02-20 07:50:19 +0300

    feature: start and stop

--- content.js
+++ content.js
@@ -1,16 +1,52 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.command === 'start') {
-    const elements = document.getElementsByClassName('grid-row ng-scope')
-    const loads = Array.from(elements).map(element => {
-      const rates = Array.from(element.getElementsByClassName('rate-bold'))
-      const price = rates.length && rates[0].innerText
-      const code = element.querySelector('[data-bo-text="e.firm.id"]').innerText
-      const routeDistance = element.getElementsByClassName('route-distance').item(0).innerText
-      return {
-        price,
-        id: `${code}-${routeDistance}`
-      }
-    })
-    console.log(elements, loads)
+const PERIOD = 10000
+const WAIT_AFTER_CLICK = 2000
+
+const getLoadsElements = () => Array.from(document.getElementsByClassName('grid-row ng-scope'))
+
+const getLoadFromElement = element => {
+  const rates = Array.from(element.getElementsByClassName('rate-bold'))
+  const price = rates.length && rates[0].innerText
+  const code = element.querySelector('[data-bo-text="e.firm.id"]').innerText
+  const routeDistance = element.getElementsByClassName('route-distance').item(0).innerText
+  return {
+    price,
+    id: `${code}-${routeDistance}`
+  }
+}
+
+const getLoads = () => {
+  const elements = getLoadsElements()
+  return elements.map(getLoadFromElement)
+}
+
+const clickOnSearch = () => {
+  const searchButton = document.getElementsByClassName("search-form-button").item(0)
+  searchButton.click()
+}
+
+let intervalId = null
+
+chrome.runtime.onMessage.addListener(({ command, question }, sender, sendResponse) => {
+  if (command === 'start') {
+    const initialLoads = getLoads()
+    intervalId = setInterval(() => {
+      clickOnSearch()
+      setTimeout(() => {
+        const elements = getLoadsElements()
+        elements.forEach(element => {
+          const load = getLoadFromElement(element)
+          const same = initialLoads.find(il => il.price === load.price && il.id === load.id)
+          if (same) {
+            element.remove()
+          }
+        })
+      }, WAIT_AFTER_CLICK)
+    }, PERIOD)
+  } else if (command === 'stop') {
+    clearInterval(intervalId)
+  }
+  if (question === 'isRunning') {
+    sendResponse(!!intervalId)
   }
 })
+
--- popup.js
+++ popup.js
@@ -1,9 +1,37 @@
 const startButton = document.getElementById('start')
+const stopButton = document.getElementById('stop')
+
+const hideButton = button => {
+  button.style.display = "none"
+}
+
+const showButton = button => {
+  button.style.display = "block"
+}
+
+const sendQuestion = (question, onResponse) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { question }, onResponse)
+  })
+}
+
+sendQuestion('isRunning', isRunning => {
+  hideButton(isRunning ? startButton : stopButton)
+})
+
+const sendCommand = command => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { command })
+  })
+}
 
 startButton.onclick = () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { command: 'start' }, (response) => {
-      console.log(response.farewell)
-    })
-  })
+  sendCommand('start')
+  hideButton(startButton)
+  showButton(stopButton)
+}
+
+stopButton.onclick = () => {
+  sendCommand('stop')
+  hideButton(stopButton)
 }
